a8b33b87748809c532b47112331095b639e8e61f
Pham Kieu Thanh <thanhpk@live.com>
2017-06-02 19:48:46 +0700

    use value intead of pointer, add more test for number

--- golang/main_test.go
+++ golang/main_test.go
@@ -11,7 +11,6 @@ func TearUp(t *testing.T) {
 
 func TestEmpty(t *testing.T) {
 	TearUp(t)
-
 	var ret bool
 	ret = ts.Evaluate("boolean", nil, Empty, nil)
 	if ret == false {
@@ -28,7 +27,7 @@ func TestEmpty(t *testing.T) {
 		t.Fatal("must be true")
 	}
 
-	ret = ts.Evaluate("set of string", nil, Empty, nil)
+	ret = ts.Evaluate("set of strings", nil, Empty, nil)
 	if ret == false {
 		t.Fatal("must be true")
 	}
@@ -37,12 +36,12 @@ func TestEmpty(t *testing.T) {
 func TestBool(t *testing.T) {
 	TearUp(t)
 	var truestr = "true"
-	ret := ts.Evaluate("boolean", &truestr, True, nil)
+	ret := ts.Evaluate("boolean", truestr, True, nil)
 	if ret == false {
 		t.Fatal("must be true")
 	}
 
-	ret = ts.Evaluate("boolean", &truestr, False, nil)
+	ret = ts.Evaluate("boolean", truestr, False, nil)
 	if ret == true {
 		t.Fatal("must be false")
 	}
@@ -58,24 +57,24 @@ func TestString(t *testing.T) {
 	var ret bool
 	str := "ab"
 	obj := "abc"
-	ret = ts.Evaluate("string", &obj, StartsWith, str)
+	ret = ts.Evaluate("string", obj, StartsWith, str)
 	if ret == false {
 		t.Fatal("must be true")
 	}
 	str = "a.c"
 	obj = "abc"
-	ret = ts.Evaluate("string", &obj, Regex, str)
+	ret = ts.Evaluate("string", obj, Regex, str)
 	if ret == false {
 		t.Fatal("must be true")
 	}
 	obj = "abc"
-	ret = ts.Evaluate("string", &obj, In, []string{"abc", "bcd"})
+	ret = ts.Evaluate("string", obj, In, []string{"abc", "bcd"})
 	if ret == false {
 		t.Fatal("must be true")
 	}
 
 	obj = "abc"
-	ret = ts.Evaluate("string", &obj, NotIn, []string{"abc", "bcd"})
+	ret = ts.Evaluate("string", obj, NotIn, []string{"abc", "bcd"})
 	if ret {
 		t.Fatal("must be false")
 	}
@@ -84,31 +83,31 @@ func TestString(t *testing.T) {
 func TestStrings(t *testing.T) {
 	TearUp(t)
 	var ret bool
-	ret = ts.Evaluate("set of string", []string{"abc"}, StartsWith, "ab")
+	ret = ts.Evaluate("set of strings", []string{"abc"}, StartsWith, "ab")
 	if !ret {
 		t.Fatal("must be true")
 	}
-	ret = ts.Evaluate("set of string", []string{"123", "abc"}, Regex, "a.c")
+	ret = ts.Evaluate("set of strings", []string{"123", "abc"}, Regex, "a.c")
 	if !ret {
 		t.Fatal("must be true")
 	}
-	ret = ts.Evaluate("set of string", []string{"abc", "efg"}, In, []string{"abc", "bcd"})
+	ret = ts.Evaluate("set of strings", []string{"abc", "efg"}, In, []string{"abc", "bcd"})
 	if !ret {
 		t.Fatal("must be true")
 	}
-	ret = ts.Evaluate("set of string", []string{"abc"}, NotIn, []string{"abc", "bcd"})
+	ret = ts.Evaluate("set of strings", []string{"abc"}, NotIn, []string{"abc", "bcd"})
 	if ret {
 		t.Fatal("must be false")
 	}
-	ret = ts.Evaluate("set of string", []string{"abc", "eft", "bcd"}, Superset, []string{"abc", "bcd"})
+	ret = ts.Evaluate("set of strings", []string{"abc", "eft", "bcd"}, Superset, []string{"abc", "bcd"})
 	if !ret {
 		t.Fatal("must be true")
 	}
-	ret = ts.Evaluate("set of string", []string{"abc"}, Subset, []string{"abc", "bcd"})
+	ret = ts.Evaluate("set of strings", []string{"abc"}, Subset, []string{"abc", "bcd"})
 	if !ret {
 		t.Fatal("must be true")
 	}
-	ret = ts.Evaluate("set of string", []string{"abc", "bcd"}, Eq, []string{"abc", "bcd"})
+	ret = ts.Evaluate("set of strings", []string{"abc", "bcd"}, Eq, []string{"abc", "bcd"})
 	if !ret {
 		t.Fatal("must be true")
 	}
@@ -119,25 +118,35 @@ func TestNumber(t *testing.T) {
 	var ret bool
 	str := "-20.0"
 	obj := "-20"
-	ret = ts.Evaluate("number", &obj, Eq, str)
+	ret = ts.Evaluate("number", obj, Eq, str)
 	if !ret {
 		t.Fatal("must be true")
 	}
 	str = "1"
 	obj = "5.4"
-	ret = ts.Evaluate("number", &obj, Gt, str)
+	ret = ts.Evaluate("number", obj, Gt, str)
 	if !ret {
 		t.Fatal("must be true")
 	}
 	obj = "5"
-	ret = ts.Evaluate("number", &obj, In, []string{"25", "5"})
+	ret = ts.Evaluate("number", obj, In, []int{25, 5})
 	if !ret {
 		t.Fatal("must be true")
 	}
 
+	ret = ts.Evaluate("number", obj, NotIn, []string{"25.0", "5.0"})
+	if ret {
+		t.Fatal("must be false")
+	}
+
 	obj = "4"
-	ret = ts.Evaluate("number", &obj, InRange, []string{"1", "20"})
+	ret = ts.Evaluate("number", obj, InRange, []int{1, 20})
 	if !ret {
+		t.Fatal("must be true")
+	}
+
+	ret = ts.Evaluate("number", obj, NotInRange, []string{"1", "20"})
+	if ret {
 		t.Fatal("must be true")
 	}
 }
