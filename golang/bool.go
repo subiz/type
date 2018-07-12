97896326194125bc347f4d1178317b70700dfcec
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2018-07-12 18:08:27 +0700

    fix bool condition

--- golang/bool.go
+++ golang/bool.go
@@ -17,17 +17,24 @@ func NewBoolType() *BoolType {
 // Evaluate operator(object, values) == true
 func (t *BoolType) Evaluate(obj interface{}, operator string, values string) bool {
 	var pobj string
+	var err error
+	var object bool
+
 	if obj != nil {
+		if _, ok := obj.(bool); ok {
+			if obj.(bool) {
+				obj = "true"
+			} else {
+				obj = "false"
+			}
+		}
 		var ok bool
 		pobj, ok = obj.(string)
 		if !ok {
-			fmt.Printf("type/golang/bool.go: obj must be a string, got `%s`\n", obj)
+			fmt.Printf("type/golang/bool.go: obj must be a string or a bool, got `%v`\n", obj)
 			return false
 		}
-	}
-	var err error
-	var object bool
-	if obj != nil {
+
 		object, err = strconv.ParseBool(pobj)
 		if err != nil {
 			fmt.Printf("type/golang/bool.go: %v unable to parse value `%s` to bool\n", err, pobj)
@@ -50,7 +57,7 @@ func (t *BoolType) Evaluate(obj interface{}, operator string, values string) boo
 		return object
 	case False:
 		if obj == nil {
-			return false
+			return true
 		}
 		return !object
 	default:
